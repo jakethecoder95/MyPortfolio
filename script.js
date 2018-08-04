bcb3b1821fe7c961a9eed3564680b35d7b6e64d7
jakethecoder95 <95jacob07@gmail.com>
2018-08-04 04:16:51 -0700

    Projects section finished!

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function alignWidth() {
+    var imageWidth = document.getElementsByClassName('project-img').offsetWidth;
+    var headers = document.getElementsByClassName('project-subheader');
+    for(i=0; i<headers.length; i++) {
+        headers[i].style.width = imageWidth;
+    }
+}
+
 function runCode() {
     alert("hello world");
 }
